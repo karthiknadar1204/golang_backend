fe2b3257d546f7a2481a4510a833b3e2d1494dcd
karthik <karthiknadar1204@gmail.com>
2025-05-24 14:56:46 +0530

    basic http server setup and logger

---
+++ cmd/api/health.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
